14443e6a3aa5904a119cdca4e60565c63f9a756f
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-20 13:39:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,3 +1,14 @@
 def key_for_min_value(name_hash)
-  
+  min_value = nil
+  min_key = nil
+  name_hash.each do |key, value|
+    if min_value == nil
+      min_value = value 
+      min_key = key 
+    elsif min_value > value 
+      min_value = value 
+      min_key = key 
+    end
+  end
+  min_key
 end
